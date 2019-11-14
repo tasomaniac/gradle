58fcada797de16e469a55018bf1b0d2cd2bca861
Stefan Wolf <wolf@gradle.com>
2019-11-14 16:00:32 +0100

    Remove compareToPrefix
    and use compareWithCommonPrefix instead.

--- subprojects/snapshots/src/main/java/org/gradle/internal/snapshot/PathUtil.java
+++ subprojects/snapshots/src/main/java/org/gradle/internal/snapshot/PathUtil.java
@@ -20,7 +20,6 @@ import com.google.common.annotations.VisibleForTesting;
 
 import java.io.File;
 import java.util.Comparator;
-import java.util.function.IntUnaryOperator;
 
 import static org.gradle.internal.snapshot.CaseSensitivity.CASE_INSENSITIVE;
 import static org.gradle.internal.snapshot.CaseSensitivity.CASE_SENSITIVE;
@@ -263,47 +262,8 @@ public class PathUtil {
         return endOfThisSegment == pathLength || isFileSeparator(absolutePath.charAt(endOfThisSegment));
     }
 
-    /**
-     * Determines whether a relative path has the given prefix,
-     * and returns the comparison of prefix and the path if it has not.
-     *
-     * Similar to {@link #isPrefix}, only that it returns whether the path is bigger or smaller than the prefix.
-     *
-     * Examples:
-     *   some/start == some/start/subpath
-     *   some/start == some/start
-     *   some/a     < some/start
-     *   some/b     > some/a
-     *
-     * @param prefix prefix to compare to
-     * @param absolutePath + offset: the relative path tested for whether it has the prefix
-     */
-    public static int compareToPrefix(String prefix, String absolutePath, int offset, CaseSensitivity caseSensitivity) {
-        int pathLength = absolutePath.length();
-        int prefixLength = prefix.length();
-        int endOfThisSegment = prefixLength + offset;
-        if (pathLength < endOfThisSegment) {
-            return comparePaths(prefix, absolutePath, offset, caseSensitivity);
-        }
-        return comparePathRegions(prefix, absolutePath, offset, prefixLength, caseSensitivity == CASE_SENSITIVE,
-            accumulatedValue -> endOfThisSegment == pathLength || isFileSeparator(absolutePath.charAt(endOfThisSegment))
-                ? accumulatedValue
-                : -1);
-    }
-
     private static int comparePaths(String relativePath, String absolutePath, int offset, CaseSensitivity caseSensitivity) {
         int maxPos = Math.min(relativePath.length(), absolutePath.length() - offset);
-        return comparePathRegions(relativePath, absolutePath, offset, maxPos, caseSensitivity == CASE_SENSITIVE,
-            accumulatedValue -> {
-                int lengthCompare = Integer.compare(relativePath.length(), absolutePath.length() - offset);
-                return lengthCompare != 0
-                    ? lengthCompare
-                    : accumulatedValue;
-            }
-        );
-    }
-
-    private static int comparePathRegions(String relativePath, String absolutePath, int offset, int maxPos, boolean caseSensitive, IntUnaryOperator andThenCompare) {
         int accumulatedValue = 0;
         for (int pos = 0; pos < maxPos; pos++) {
             char charInPath1 = relativePath.charAt(pos);
@@ -312,12 +272,15 @@ public class PathUtil {
             if (comparedChars != 0) {
                 return comparedChars;
             }
-            accumulatedValue = computeCombinedCompare(accumulatedValue, charInPath1, charInPath2, caseSensitive);
+            accumulatedValue = computeCombinedCompare(accumulatedValue, charInPath1, charInPath2, caseSensitivity == CASE_SENSITIVE);
             if (accumulatedValue != 0 && isFileSeparator(charInPath1)) {
                 return accumulatedValue;
             }
         }
-        return andThenCompare.applyAsInt(accumulatedValue);
+        int lengthCompare = Integer.compare(relativePath.length(), absolutePath.length() - offset);
+        return lengthCompare != 0
+            ? lengthCompare
+            : accumulatedValue;
     }
 
     private static int computeCombinedCompare(int previousCombinedValue, char charInPath1, char charInPath2, boolean caseSensitive) {
